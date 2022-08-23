6a8c57b64fa79eadc6c4cb4e0b9c8c9d585e3fdc
Foddie2 <Bensonagala@live.com>
2022-08-23 17:49:26 +0300

    debugs

--- components/Blog.vue
+++ components/Blog.vue
@@ -13,7 +13,8 @@
             <div
               class="absolute inset-0 bg-gray-800 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none"
               aria-hidden="true"
-            ></div>
+            >
+            </div>
             <figure
               class="relative h-0 pb-[56.25%] md:pb-[75%] lg:pb-[56.25%] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out"
             >
